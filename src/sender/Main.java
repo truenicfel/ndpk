b3137cf09ed091ef28b90dbf44503fb91508dae3
konopac <konopac@hm.edu>
2016-12-17 19:16:41 +0100

    Main.java finished (all error cases are catched, doku finished), small changes in Sender.java (doku, new method send(), new textfile to be send in files/file.txt

--- src/sender/Main.java
+++ src/sender/Main.java
@@ -1,10 +1,137 @@
 package sender;
 
+import java.io.File;
+import java.nio.file.InvalidPathException;
+
 public class Main {
 	
-	// The program starts here
+	/**
+	 * <b>The program starts here.</b><br>
+	 * Call the program with argument "-help" to see the use instructions,
+	 * otherwise two arguments are needed:<br>
+	 * <br>
+	 * 1. <b>File Name</b> (including the path, if program does not start in the same folder as the file<br>
+	 * 2. <b>Destination</b> IP-Address or "localhost"<br>
+	 * 
+	 * @param args contains the given arguments
+	 */
 	public static void main(String[] args) {
-		
+		// check if help is needed
+		if ("-help".equals(args[0])) {
+			// show help
+			showHelp();
+		} 
+		// check number of given arguments
+		else if (args.length != 2) {
+			showError(
+					"The number of arguments must be 2, but was " + args.length + "."
+					+ "\r\n"
+					+ "If you need help, start the program with the -help argument."
+					);
+		}
+		// check if second argument is a IPv4 address or "localhost"
+		else if (!validIpv4(args[1]) && !"localhost".equals(args[1])) {
+			showError(
+					"The given IPv4-Address is invalid. It should be something like \"192.168.2.1\"."
+					+ "\r\n"
+					+ "If you need help, start the program with the -help argument.");
+		}
+		// check if the given file exists
+		else if (!new File(args[0]).exists()) {
+			showError(
+					"The named File \"" + args[0] + "\" can not be found."
+					+ "\r\n"
+					+ "If you need help, start the program with the -help argument."
+					);
+		}
+		// check if the given file is a directory
+		else if (new File(args[0]).isDirectory()) {
+			showError(
+					"\"" + args[0] + "\" is a directory and not a file."
+					+ "\r\n"
+					+ "If you need help, start the program with the -help argument."
+					);
+		}
+		// everything is ok
+		else {
+			// get values from arguments
+			final String fileName = args[0];
+			final String destination = "localhost".equals(args[1]) ? "127.0.0.1" : args[1];
+			// create new sender object, new File(fileName).toPath() might throw an exception
+			final Sender sender = new Sender(new File(fileName).toPath(), destination);
+			// start sending
+			sender.send();
+		}
+	}
+	
+	/**
+	 * <b>Print the use instructions.</b>
+	 */
+	private static void showHelp() {
+		System.out.println(
+				""
+				+ "####################\r\n"
+				+ "# USE INSTRUCTIONS #\r\n"
+				+ "####################\r\n"
+				+ "\r\n"
+				+ "The program has to be called with two arguments:\r\n"
+				+ "\r\n"
+				+ "1. File Name\r\n"
+				+ "\tThis argument is the complete File Name of the File to be send.\r\n"
+				+ "\tIf the program does not start in the files direktory, you have\r\n"
+				+ "\tto use the relative path from the program to the file.\r\n"
+				+ "\tYou can reach the parent folder using \"../\" in the path.\r\n"
+				+ "\tExample File Name: \"../../Folder/File.txt\"\r\n"
+				+ "\r\n"
+				+ "2. Destination\r\n"
+				+ "\tThis argument is the IPv4-Address of the receiver.\r\n"
+				+ "\tIf the receiver is localhost, you can just type \"localhost\"\r\n"
+				+ "\tinstead of the IP-Address \"127.0.0.1\"\r\n"
+				+ "\tExample IPv4-Address: \"192.169.2.1\"\r\n"
+				+ "\r\n");
+	}
+	
+	/**
+	 * <b>Print the given Error Message.</b>
+	 * 
+	 * @param msg is the given message
+	 */
+	private static void showError(String msg) {
+		System.err.println(msg);
+	}
+	
+	/**
+	 * <b>Check, if the argument is a valid IP-Address.</b>
+	 * 
+	 * @param ip is the given IP-Address.
+	 * @return true, if the IP-Address is valid
+	 */
+	private static boolean validIpv4(String ip) {
+		// store the result
+		boolean result = false;
+		// split the IP-Address at the dots
+		final String[] splitted = ip.split(".");
+		// a IPv4-Address should split in 4 elements
+		if (splitted.length == 4) {
+			// set result true and check all elements later
+			result = true;
+			// loop through all elements
+			for (final String element : splitted) {
+				try {
+					// parse element to int (might throw an exception)
+					final int elementAsInt = Integer.parseInt(element);
+					// check if the element is NOT a number from 0 to 255
+					if ((elementAsInt < 0) || (255 < elementAsInt)) {
+						// IP-Address is invalid
+						result = false;
+					}
+				} catch(NumberFormatException exception) {
+					// if there was a parsing error, the IP-Address is invalid
+					result = false;
+				}
+			}
+		}
+		return result;
 	}
 
 }
--- src/sender/Sender.java
+++ src/sender/Sender.java
@@ -3,18 +3,17 @@ package sender;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 import protocolPackets.AlternatingBitPacket;
 
 /**
- * Sender class
+ * <b>Sender class.</b>
  */
 public class Sender extends Thread {
 	
 	/**
-	 * The states, that Sender can have
+	 * <b>The states, that Sender can have.</b>
 	 */
 	private enum State {
 		waitForCall0,
@@ -24,25 +23,25 @@ public class Sender extends Thread {
 	}
 	
 	/**
-	 * Current state
+	 * Current state.
 	 */
 	private State currentState;
 	
 	/**
-	 * File to be send
+	 * File to be send.
 	 */
 	private final Path file;
 	
 	/**
-	 * IP-Adress of the receiver
+	 * IP-Address of the receiver.
 	 */
 	private final String ipAdress;
 	
 	/**
-	 * Constructor with following params
+	 * <b>Constructor with following params.</b>
 	 * 
-	 * @param file
-	 * @param ipAdress
+	 * @param file is the file to be send
+	 * @param ipAdress is the IP-Address of the receiver
 	 */
 	public Sender(Path file, String ipAdress) {
 		this.file = file;
@@ -53,7 +52,7 @@ public class Sender extends Thread {
 	}
 	
 	/**
-	 * Reads the given file and sends it to the given receiver
+	 * <b>Reads the given file and sends it to the given receiver.</b>
 	 * 
 	 * @see java.lang.Thread#run()
 	 */
@@ -61,7 +60,7 @@ public class Sender extends Thread {
 	public void run() {
 		try {
 			// read file in byte array
-			final byte[] data = Files.readAllBytes(file);
+			final byte[] data = Files.readAllBytes(getFile());
 			
 			// split data in packets
 			for (int start = 0; start < data.length; start += AlternatingBitPacket.PACKAGESIZE) {
@@ -69,12 +68,57 @@ public class Sender extends Thread {
 				final byte[] packet = Arrays.copyOfRange(data, start, start + AlternatingBitPacket.PACKAGESIZE <= data.length ?  start + AlternatingBitPacket.PACKAGESIZE : data.length);
 			}
 			
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException exception) {
+			exception.printStackTrace();
 		}
 	
 	
 	}
 	
+	/**
+	 * <b>Start sending the file.</b>
+	 * 
+	 * This method just calls the Thread.start() method, but still is preferred.
+	 */
+	public void send() {
+		start();
+	}
 	
+	
+	
+	/**
+	 * <b>Get the current state.</b>
+	 * 
+	 * @return the current state
+	 */
+	private State getCurrentState() {
+		return currentState;
+	}
+	
+	/**
+	 * <b>Set the current state.</b>
+	 * 
+	 * @param currentState is the new current state
+	 */
+	private void setCurrentState(State currentState) {
+		this.currentState = currentState;
+	}
+	
+	/**
+	 * <b>Get the file to be send.</b>
+	 * 
+	 * @return the file
+	 */
+	private Path getFile() {
+		return file;
+	}
+
+	/**
+	 * <b>Get IP-Adress of the receiver.</b>
+	 * 
+	 * @return the IP-Address
+	 */
+	private String getIpAdress() {
+		return ipAdress;
+	}
 }
